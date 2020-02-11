8d934801b34aaf62e8e16cdf0a341a4f2d90aa91
Daphne <daphne.feresin@gmail.com>
2020-02-11 17:02:30 -0500

    updates

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 let createName = function (first, last){
 return first + " "+last
 }
+
+let askName = function (){
+    let first = prompt("Please enter your first name", "First Name");
+    let last = prompt("Please enter your last name", "Last Name");
+    let fullName = createName(first,last)
+    alert("Your name is "+fullName)
+}
+
+askName()
